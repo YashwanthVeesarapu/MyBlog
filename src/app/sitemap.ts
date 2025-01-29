a3ade48abf42f563cf2883d35717d09f36442bec
Yashwanth Reddy Veesarapu <yashwanthreddyveesarapu@gmail.com>
2025-01-29 14:22:34 -0500

    sitemap changes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,7 @@ export default async function sitemap() {
   const call = await fetch(SERVER_URL, { next: { revalidate: 3600 } });
   const blogs = await call.json();
 
-  const URL = "https://blog.redsols.us/";
+  const URL = "https://blog.redsols.com/";
 
   const currentDate = new Date();
   const formattedDate = currentDate.toISOString().split("T")[0];
