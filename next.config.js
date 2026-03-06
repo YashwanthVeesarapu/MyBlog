4de1b251c561476445144e73bf0c3417c1e90ae4
Yashwanth Veesarapu <yashwanthreddyveesarapu@gmail.com>
2026-03-05 23:28:11 -0500

    chore: Add `isomorphic-dompurify` and `@exodus/bytes` to Next.js transpile packages configuration.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   output: "export",
   trailingSlash: true,
   distDir: "dist",
+  transpilePackages: ["isomorphic-dompurify", "@exodus/bytes"],
 };
 
 module.exports = nextConfig;
