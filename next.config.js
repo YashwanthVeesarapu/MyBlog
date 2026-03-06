14b92c4381248d384f66283316f866b5382528e5
Yashwanth Veesarapu <yashwanthreddyveesarapu@gmail.com>
2026-03-05 23:33:53 -0500

    build: Update CI Node.js version to 22.x and remove `transpilePackages` from Next.js configuration.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   output: "export",
   trailingSlash: true,
   distDir: "dist",
-  transpilePackages: ["isomorphic-dompurify", "@exodus/bytes"],
 };
 
 module.exports = nextConfig;
